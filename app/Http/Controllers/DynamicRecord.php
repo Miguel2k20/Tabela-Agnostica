f1cc6eddde18321a223f481b3854ee0c16e60ecd
Miguel2k20 <joaomiguel2k20@gmail.com>
2026-04-26 19:52:56 -0300

    Criação de registro

--- app/Http/Controllers/DynamicRecord.php
+++ app/Http/Controllers/DynamicRecord.php
@@ -3,12 +3,86 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\DynamicRecord as ModelsDynamicRecord;
+use App\Models\ModuleDefinition;
 use Illuminate\Http\Request;
 
 class DynamicRecord extends Controller
 {
-    public function store() {
-        
+    public function store(Request $request, ModuleDefinition $module) 
+    {
+        if (!$module->exists) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Módulo não encontrado.',
+            ], 404);
+        }
+
+        try {
+            $schema = $module->schema_json;
+            $rules = [];
+
+
+            foreach ($schema as $fieldName => $field) 
+            {
+                $rule = [];
+
+                $rule[] = ($field['required'] ?? false) ? 'required' : 'nullable';
+                
+                switch ($field['type'] ?? 'string') {
+                    case 'string':
+                    case 'text':
+                        $rule[] = 'string';
+                        $rule[] = 'max:255';
+                        break;
+
+                    case 'int':
+                    case 'integer':
+                        $rule[] = 'integer';
+                        break;
+
+                    case 'float':
+                        $rule[] = 'numeric';
+                        break;
+
+                    case 'boolean':
+                        $rule[] = 'boolean';
+                        break;
+
+                    case 'date':
+                        $rule[] = 'date_format:d/m/Y';
+                        break;
+
+                    case 'enum':
+                        $rule[] = 'string';
+                        break;
+
+                    default:
+                        $rule[] = 'string';
+                        break;
+                }
+                $rules[$fieldName] = $rule;
+            }
+                
+            $validated = $request->validate($rules);
+
+
+            $record = ModelsDynamicRecord::create([
+                'reference' => $module->reference,
+                'dados' => $validated 
+            ]);
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Registro criado com sucesso.',
+                'data' => $record
+            ], 201);
+
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Erro ao criar registro: ' . $e->getMessage(),
+            ], 400);
+        }
     }
-    //
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ModuleDefinition;
+use App\Http\Controllers\DynamicRecord;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -12,3 +13,5 @@ Route::post('/module', [ModuleDefinition::class, 'store']);
 Route::delete("/module/{module}", [ModuleDefinition::class, 'delete']);
 Route::put("/module/{module}", [ModuleDefinition::class, 'update']);
 Route::get('/modules', [ModuleDefinition::class, 'index']);
+
+Route::post('/dynamic-record/{module}', [DynamicRecord::class, 'store']);
